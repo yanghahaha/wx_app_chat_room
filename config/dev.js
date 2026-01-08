bc20ba265f84bb0f5c6a7e5581a7abaebcfa7ae1
woooo <yanghahaha@126.com>
2026-01-08 22:38:58 +0800

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,9 +1,9 @@
 module.exports = {
     MYSQL_CONFIG: {
-        HOST: '111.231.209.249',
-        NAME: 'wx_chat_room',
-        USER: 'yangwu',
-        PWD: 'dsf651gdfvsad12344',
+        HOST: '',
+        NAME: '',
+        USER: '',
+        PWD: '',
     },
-    LIVE_API_HOST: 'http://test.liveapi.ejudata.com',
+    LIVE_API_HOST: 'http://m',
 }
